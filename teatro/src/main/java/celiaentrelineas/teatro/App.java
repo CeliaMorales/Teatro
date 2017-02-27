f76affb027194dab91a5300d0ef5da7de3a4be2b
ARELANCE\celia.morales <celiia.morales.martinez.93@gmail.com>
2017-02-27 09:17:21 +0100

    Subida inicial

---
+++ teatro/src/main/java/celiaentrelineas/teatro/App.java
@@ -0,0 +1,13 @@
+package celiaentrelineas.teatro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
